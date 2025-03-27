94c10aa662b1304c668f6acfa7fadf2b017965f8
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2025-03-27 14:44:36 +0530

    Namespace Library

---
+++ C#/Practice Questions/NamespaceLibrary/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialCalculation
+{
+    public class Math
+    {
+    }
+}
+
+namespace Calculation
+{
+    public class Math
+    {
+    }
+}

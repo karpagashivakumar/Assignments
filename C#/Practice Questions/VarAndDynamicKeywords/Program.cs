a6f47ff68ca9037c743bf2ab2c3216e6e69ff693
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2025-03-27 14:47:38 +0530

    Var and Dynamic Keywords

---
+++ C#/Practice Questions/VarAndDynamicKeywords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarAndDynamicKeywords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            Console.WriteLine("Get type of i : " + i.GetType());
+
+            dynamic j = "Hello";
+            Console.WriteLine("Get type of j : " + j.GetType());
+
+            Console.ReadKey();
+        }
+    }
+}

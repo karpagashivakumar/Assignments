b90a4f2c13cf2429e3e5408e810ac866f9313005
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2025-03-27 14:42:06 +0530

    Class Library

---
+++ C#/Practice Questions/FirstDemoLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemoLibrary
+{
+    public class Class1
+    {
+    }
+    public class Class2
+    {
+    }
+    public class Class3
+    {
+    }
+}

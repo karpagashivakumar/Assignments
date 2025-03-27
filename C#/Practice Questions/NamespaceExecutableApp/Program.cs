241b1b7fc8af2a9081789d2df74929bb6877c760
Karpagashivakumar <113433827+karpagashivakumar@users.noreply.github.com>
2025-03-27 14:45:19 +0530

    Executable file for Namespace

---
+++ C#/Practice Questions/NamespaceExecutableApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using FinancialCalculation;
+using Mathematical;
+using Calculation;
+using NamespaceLibrary;
+
+namespace NamespaceExecutableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FinancialCalculation.Math mathObject = new FinancialCalculation.Math();
+            Mathematical.Math mathFunc = new Mathematical.Math();
+            Calculation.Math mathCalc = new Calculation.Math();
+            Class1 obj = new Class1();
+
+            Console.WriteLine(mathObject);
+            Console.WriteLine(mathFunc);
+            Console.WriteLine(mathCalc);
+
+            FinancialCalculation.Math mathObject2 = new FinancialCalculation.Math();
+            Mathematical.Math mathFunc2 = new Mathematical.Math();
+            Calculation.Math mathCalc2 = new Calculation.Math();
+        }
+    }
+}
